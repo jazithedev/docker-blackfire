3fef3f1dbe578a718f8231ce6e80bd5f7af6b208
Krzysztof Trzos <k.trzos@jazi.pl>
2018-05-03 14:36:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Example of Docker container having php, nginx and Blackfire.io profiler.
+
+Example application on: http://l.domain
---
+++ www/domain/index.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>This is domaaaaain!</title>
+</head>
+<body>
+    <p><?php echo 'Hello world of domains!'; ?></p>
+</body>
+</html>
